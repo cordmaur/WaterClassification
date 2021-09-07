96ac49b4fe533fb40bac55a30d081f33e83c7833
Mauricio Cordeiro <cordmaur@gmail.com>
2021-09-07 16:27:12 +0200

    Refactor 1

---
+++ WaterClassification/Radiometry/__init__.py
@@ -0,0 +1,4 @@
+# This package needs the HyperspectralTrios to be installed
+from HyperspectralTrios.radiometry import Radiometry, BaseRadiometry
+from HyperspectralTrios.radiometry_db import RadiometryDB
+
